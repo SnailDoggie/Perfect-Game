1e17d9db1ffafe4f33c67215c35ab4911949fd87
SnailDoggie <bakkacla000@edmonds15.org>
2026-08-29 14:02:11 -0700

    update

--- jimothy.gd
+++ jimothy.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -450.0
+const JUMP_VELOCITY = -480.0
 @onready var player_sprite = $Sprite2D
 
 func _physics_process(delta: float) -> void:
